1902ba05445eb097713b1966806014b72d2390fa
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2020-05-11 15:23:18 -0300

    Numerologia: Adicionado calculadora de nomes

--- src/app/numerologia/numerologia.component.ts
+++ src/app/numerologia/numerologia.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumerologiaComponent implements OnInit {
 
+  nome: string = '';
+  valores: { letra: string, numero: number }[] = [];
+  final: { numero: number, soma: number };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calcularLetras(refNome: string) {
+    const nome = refNome.toUpperCase();
+    let total = 0;
+    let valores = [];
+    for (let i = 0; i < nome.length; i++) {
+      let letra = nome[i];
+      let numero = this.sumDigits(letra.charCodeAt(0) - 64);
+      total += numero;
+      valores.push({ letra: letra, numero: numero });
+    }
+
+    this.valores = valores;
+    this.final = { numero: total, soma: this.sumDigits(total) };
+  }
+
+  sumDigits(n) {
+    return (n - 1) % 9 + 1;
   }
 
 }
