f0ac1a2cf4b2d9b0a02936624e4647d6bdb77797
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2024-04-24 11:16:41 -0300

    build bat to clean build with npm command

---
+++ build.bat
@@ -0,0 +1,3 @@
+del /s /q C:\Users\Emanuel\Documents\GitHub\emanuelumbelino.github.io\docs\*
+
+xcopy "C:\Users\Emanuel\Documents\GitHub\emanuelumbelino.github.io\dist\browser" "C:\Users\Emanuel\Documents\GitHub\emanuelumbelino.github.io\docs\" /E /Q
