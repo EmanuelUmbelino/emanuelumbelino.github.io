3a1c3ac8078aadc6e394035835c4f0ae9de79014
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2024-04-23 21:48:10 -0300

    details and tests with work experience timeline

--- src/app/modules/portfolio/components/about-me/about-me.component.ts
+++ src/app/modules/portfolio/components/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WorkExperience } from '@modules/portfolio/models/work-experience.model';
 import { CalculateAge } from '@shared/helpers/calculate-age';
 
 @Component({
@@ -9,8 +10,36 @@ import { CalculateAge } from '@shared/helpers/calculate-age';
 export class AboutMeComponent {
 
   birthDate = new Date('08/15/1998');
-
   currYear = new Date().getFullYear();
 
   calculateAge = CalculateAge;
+
+  workExperience: WorkExperience[] = [
+    {
+      company: 'VONEX',
+      position: 'Senior Front End Developer',
+      tecnology: 'Angular 16',
+      startDate: new Date(2022,8),
+    },
+    {
+      company: 'PUC-Rio',
+      position: 'Front End Developer',
+      tecnology: 'Angular 8 / 14',
+      startDate: new Date(2019,11),
+    },
+    {
+      company: 'FIOCRUZ',
+      position: 'Application Developer',
+      tecnology: 'Vue.js',
+      startDate: new Date(2018,9),
+      endDate: new Date(2019,11),
+    },
+    {
+      company: 'IBM',
+      position: 'Research Intern',
+      tecnology: 'Angular 6',
+      startDate: new Date(2018,9),
+      endDate: new Date(2019,10),
+    },
+  ];
 }
