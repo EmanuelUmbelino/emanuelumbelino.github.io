e70ca580ce111a1dd1bdf6db881e30252567a57e
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2025-09-01 17:18:50 -0300

    update github pages by bat

---
+++ github-pages.bat
@@ -0,0 +1,6 @@
+cd dist/emanuelumbelino.github.io/browser
+
+git init
+git add .
+git commit -m "New Deploy to GitHub Pages"
+git push --force "https://github.com/EmanuelUmbelino/emanuelumbelino.github.io.git" master:gh-pages
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('emanuelumbelino.github.io');
+  protected readonly title = signal('Emanuel');
 }
